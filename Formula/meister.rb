0d3f404a4c7a38792b38906d188f4c23e4715218
Marco Föllmer <marco@merados.com>
2026-04-16 23:23:48 +0200

    formula: update SHA256 for v4.2

--- Formula/meister.rb
+++ Formula/meister.rb
@@ -1,10 +1,10 @@
 class Meister < Formula
   desc "macOS Maintenance, Self-Healing & Dotfiles Sync"
   homepage "https://github.com/maf4711/homebrew-meister"
-  url "https://github.com/maf4711/homebrew-meister/archive/refs/tags/v4.1.tar.gz"
-  sha256 "bb6fba58597d46d7aa63abead95881ab18e61a683d66e007aaad88fc33f77930"
+  url "https://github.com/maf4711/homebrew-meister/archive/refs/tags/v4.2.tar.gz"
+  sha256 "941801d08ce977e79a49b094d66625cf5dac17b4d1ca80e2d201f69ddd98fcc0"
   license "GPL-3.0-only"
-  version "4.1"
+  version "4.2"
 
   depends_on :macos
 
